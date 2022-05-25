b00e146054722e02f17287bc14b4bedbac764dad
Geo-the-Pitbull <92966244+Geo-the-Pitbull@users.noreply.github.com>
2022-05-24 21:26:03 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+//Initializing the Local Port
+const port = process.env.PORT || 4200;
+
+//Base Variables for Express
+var path = require('path');
+var express = require('express');
+var flash = require('express-flash');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+var app = express();
+
+var conn = require('./lib/db');
+
+//---ROUTES---//
+
+//---VIEWS---//
+
+//---MIDDLEWARES---//
+
+app.listen(port, () => console.log('Listening on port ${port}..'));
